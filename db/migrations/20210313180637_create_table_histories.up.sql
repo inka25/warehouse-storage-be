530092e3f9a8ff6659955a8a12e21fb1e57e246e
Inka <inka@Inkas-MacBook-Air.local>
2023-08-11 14:57:17 +0700

    remove id from histories

--- db/migrations/20210313180637_create_table_histories.up.sql
+++ db/migrations/20210313180637_create_table_histories.up.sql
@@ -1,5 +1,4 @@
 CREATE TABLE IF NOT EXISTS `histories` (
-                             `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
                              `product_id` int(10) unsigned DEFAULT NULL,
                              `type` varchar(255) NOT NULL,
                              `previous_info` json NOT NULL,
@@ -7,7 +6,6 @@ CREATE TABLE IF NOT EXISTS `histories` (
                              `description` varchar(255) DEFAULT NULL,
                              `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
                              `updated_by` varchar(255) NOT NULL,
-                             PRIMARY KEY (`id`),
                              KEY `product_id` (`product_id`),
                              CONSTRAINT `histories_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`) ON UPDATE CASCADE
 )
